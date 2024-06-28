222c184e50a9b6f8d4eda9efafddeb4f49308217
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-06-28 09:45:58 +0100

    added lives and fail state after 10 misses

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
 import { generateDisplay } from "./components/generateDisplay";
-import React from 'react'
+import React from "react";
 
 function App() {
+    const [guessedLetters, setGuessedLetters] = React.useState([]);
+    const [numberOfmisses, setNumberOfMisses] = React.useState(0)
+    // const [gameInProgress, setGameInProgress] = React.useState(true)
+    const answer = 'apple'
 
-    const[guessedLetters, setGuessedLetters] = React.useState(['a'])
+    // if(numberOfmisses === 10){
+    //     setGameInProgress(false)
+    // }
+
+    const alphabetArray = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    const buttons = alphabetArray.map((letter) => {
+        return(<button key={letter}
+        disabled={guessedLetters.includes(letter) || numberOfmisses === 10}
+        onClick={()=>{
+
+            setGuessedLetters((prevArray) => [...prevArray, letter])
+            if(!answer.includes(letter)){
+                setNumberOfMisses((prevValue) => prevValue + 1)
+            }
+        }
+        }
+        >{letter}</button>)
+    })
 
     return (
         <div>
             <h1>Hangman Game</h1>
-            <h2>{generateDisplay(guessedLetters, 'apple')}</h2>
-
-            <h4>Number of misses:</h4>
-            <div className='letters'>
-                <button>a</button>
-                <button>b</button>
-                <button>c</button>
-                <button>d</button>
-                <button>e</button>
-                <button>f</button>
-                <button>g</button>
-                <button>h</button>
-                <button>i</button>
-                <button>j</button>
-                <button>k</button>
-                <button>l</button>
-                <button>m</button>
-                <button>n</button>
-                <button>o</button>
-                <button>p</button>
-                <button>q</button>
-                <button>r</button>
-                <button>s</button>
-                <button>t</button>
-                <button>u</button>
-                <button>v</button>
-                <button>w</button>
-                <button>x</button>
-                <button>y</button>
-                <button>z</button>
-        </div>
-
+            <h2>{generateDisplay(guessedLetters, answer)}</h2>
+            <h2>{numberOfmisses === 10 ? `You lose,the answer was ${answer}.`: null}</h2>
+            <h4>Number of misses: {numberOfmisses}</h4>
+            <div className="letters">
+                {buttons}
+            </div>
         </div>
     );
 }
